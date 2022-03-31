f6f8638339c942fc3c4b034df2ef471e0c8a60b0
ALEJANDRO <alejandro.lachner@gmail.com>
2022-03-31 19:03:27 +0200

    Lectura de config.json

---
+++ src/BusinessLayer/Entities/Config.java
@@ -0,0 +1,30 @@
+package BusinessLayer.Entities;
+
+public class Config {
+
+    private int dataBasePort;
+    private String dataBaseIP;
+    private String dataBaseName;
+    private String userName;
+    private String password;
+
+    public int getDataBasePort() {
+        return dataBasePort;
+    }
+
+    public String getDataBaseIP() {
+        return dataBaseIP;
+    }
+
+    public String getDataBaseName() {
+        return dataBaseName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
