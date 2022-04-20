a295eca5ee7969ece495e75f5fbb28ef087bcaab
abraham1cede <abraham.ceho@hotmail.com>
2022-04-20 13:35:48 +0200

    -Barra reproduccion [CLASE]: Por hacer -View Add Music: INCOMPLETA -View General de la configuracion: INCOMPLETA

--- src/presentation/ConfigurationMenu.java
+++ src/presentation/ConfigurationMenu.java
@@ -4,14 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ConfigurationMenu extends JFrame{
-    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-
     public ConfigurationMenu () {
         JFrame f = new JFrame("SPOTIFAI");
-
         //Configuración del frame
-        JFrame.setDefaultLookAndFeelDecorated(true);
-        f.setSize(screenSize.width, screenSize.height);
+
+        f.setLocationRelativeTo(null);
+        f.setSize(1500, 850);
+        f.setResizable(false);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Creamos el Jpanel de la izquierda y derecha con layouts
@@ -36,12 +35,13 @@ public class ConfigurationMenu extends JFrame{
         JSeparator lineSeparador3 = new JSeparator();
 
         menuPanel.setBackground(new Color(191, 105, 240));
-        menuPanel.setBounds(0, 0, 200, screenSize.height);
-        menuPanel.setLayout(new GridLayout(3, 1, 0, 3));
+        menuPanel.setBounds(0, 0, 200, 900);
+        //menuPanel.setLayout(new GridLayout(3, 1, 0, 3));
+        menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.Y_AXIS));
 
         subPanel.setBackground(new Color(191, 105, 240));
         subPanel.setBounds(0, 0, 200, 400);
-        subPanel.setLayout(new GridLayout(6, 1, 0, 3));
+        subPanel.setLayout(new BoxLayout(subPanel, BoxLayout.Y_AXIS));
 
         nuestroLogo.setText("NuestroLogo");
         botonInicio.setText("Boton");
@@ -72,7 +72,7 @@ public class ConfigurationMenu extends JFrame{
     public void createContentPanel () {
         JPanel content = new JPanel();
         content.setBackground(new Color(255, 255, 255));
-        content.setBounds(0, 0, screenSize.width, screenSize.height);
+        content.setBounds(0, 0, 1500, 900);
 
         getContentPane().add(content);
     }
