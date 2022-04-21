4b384df07324b6f8f1e8f46971f869d7dddaa112
pepeperez13 <josepehern@gmail.com>
2022-04-21 17:05:04 +0200

    SQL y vistas

--- src/presentation/controller/SignUpViewController.java
+++ src/presentation/controller/SignUpViewController.java
@@ -2,10 +2,26 @@ package presentation.controller;
 
 import presentation.view.SignUpView;
 
-public class SignUpViewController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SignUpViewController implements ActionListener {
+    private SignUpView view;
 
     public static void main (String[] args) {
         SignUpView signUpView = new SignUpView();
         signUpView.setVisible(true);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals(SignUpView.MALE_COMMAND)) {
+            // Select male
+        } else if (e.getActionCommand().equals(SignUpView.FEMALE_COMMAND)) {
+            // Select female
+        } else {
+            // Select other
+        }
+    }
+
 }
