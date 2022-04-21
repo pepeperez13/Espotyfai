ec9cbbf9cee2ad3272c5843b8b908a90aa5564a9
abraham1cede <abraham.ceho@hotmail.com>
2022-04-21 23:58:07 +0200

    -Barra reproduccion [CLASE]: Por hacer -View Add Music: INCOMPLETA -View General de la configuracion: INCOMPLETA

--- src/presentation/AddMusic.java
+++ src/presentation/AddMusic.java
@@ -2,16 +2,17 @@ package presentation;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class AddMusic extends JFrame {
     ConfigurationMenu plantilla = new ConfigurationMenu();
     BarReproduccion barraReproduccion = new BarReproduccion();
 
-    public AddMusic () {
+    public AddMusic () throws IOException {
         //plantilla.add(barraReproduccion);
     }
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws IOException {
         ConfigurationMenu add = new ConfigurationMenu();
         add.setVisible(true);
     }
--- src/presentation/ConfigurationMenu.java
+++ src/presentation/ConfigurationMenu.java
@@ -1,10 +1,14 @@
 package presentation;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class ConfigurationMenu extends JFrame{
-    public ConfigurationMenu () {
+    public ConfigurationMenu () throws IOException {
         setTitle("SPOTIFAI");
         setLocationRelativeTo(null);
         setSize(1500, 900);
@@ -18,11 +22,11 @@ public class ConfigurationMenu extends JFrame{
         createContentPanel();
     }
 
-    private void createMenuPanelFront () {
+    private void createMenuPanelFront () throws IOException {
         JPanel menuPanel = new JPanel();
-        //JPanel subPanel = new JPanel();
-        JLabel nuestroLogo = new JLabel();
-        JLabel botonInicio = new JLabel();
+        JPanel subPanel = new JPanel();
+        //JLabel nuestroLogo = new JLabel();
+        //JLabel botonInicio = new JLabel();
         JLabel gestionarCuenta = new JLabel();
         JLabel gestionarMusica = new JLabel();
         JLabel misEstadisticas = new JLabel();
@@ -30,17 +34,23 @@ public class ConfigurationMenu extends JFrame{
         JSeparator lineSeparador2 = new JSeparator();
         JSeparator lineSeparador3 = new JSeparator();
 
+        BufferedImage logo = ImageIO.read(new File("images/logo.png"));
+        BufferedImage iconInicio = ImageIO.read(new File("images/icons8-casa-24.png"));
+
+        JLabel nuestroLogo = new JLabel(new ImageIcon(logo.getScaledInstance(150, 150, 1)));
+        JLabel botonInicio = new JLabel(new ImageIcon(iconInicio.getScaledInstance(30,30, 1)));
+
         menuPanel.setBackground(new Color(191, 105, 240));
         menuPanel.setBounds(0, 0, 200, 900);
         //menuPanel.setLayout(new GridLayout(3, 1, 0, 3));
-        menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.Y_AXIS));
 
-        //subPanel.setBackground(new Color(191, 105, 240));
-        //subPanel.setBounds(0, 0, 200, 200);
+
+        subPanel.setBackground(new Color(255, 255, 0));
+        subPanel.setBounds(0, 0, 200, 200);
         //subPanel.setLayout(new BoxLayout(subPanel, BoxLayout.Y_AXIS));
 
-        nuestroLogo.setText("NuestroLogo");
-        botonInicio.setText("Boton");
+        //nuestroLogo.setText("NuestroLogo");
+        //botonInicio.setText("Boton");
         gestionarMusica.setText("Gestionar música");
         gestionarCuenta.setText("Gestionar cuenta");
         misEstadisticas.setText("Mis estadísticas");
@@ -51,28 +61,31 @@ public class ConfigurationMenu extends JFrame{
         lineSeparador3.setOrientation(SwingConstants.HORIZONTAL);
         //lineSeparador3.setSize(200, 1);
 
+        subPanel.add(gestionarCuenta);
+        subPanel.add(lineSeparador1);
+        subPanel.add(gestionarMusica);
+        subPanel.add(lineSeparador2);
+        subPanel.add(misEstadisticas);
+        subPanel.add(lineSeparador3);
+        subPanel.setLayout(new BoxLayout(subPanel, BoxLayout.Y_AXIS));
+
         menuPanel.add(nuestroLogo);
         menuPanel.add(botonInicio);
-        //menuPanel.add(subPanel);
-        menuPanel.add(gestionarCuenta);
-        menuPanel.add(lineSeparador1);
-        menuPanel.add(gestionarMusica);
-        menuPanel.add(lineSeparador2);
-        menuPanel.add(misEstadisticas);
-        menuPanel.add(lineSeparador3);
+        menuPanel.add(subPanel);
+        menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.Y_AXIS));
 
         getContentPane().add(menuPanel);
     }
 
     private void createContentPanel () {
         JPanel content = new JPanel();
-        content.setBackground(new Color(255, 255, 255));
+        content.setBackground(new Color(100, 100, 255));
         content.setBounds(200, 0, 1300, 900);
 
         getContentPane().add(content);
     }
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws IOException {
         ConfigurationMenu conf = new ConfigurationMenu();
         conf.setVisible(true);
     }
