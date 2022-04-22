d43437918d48c78f0f266ee27f79d642acb1a567
pepeperez13 <josepehern@gmail.com>
2022-04-22 08:05:33 +0200

    hola

--- src/presentation/controller/SignUpViewController.java
+++ src/presentation/controller/SignUpViewController.java
@@ -4,14 +4,17 @@ import presentation.view.SignUpView;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class SignUpViewController implements ActionListener {
+public class SignUpViewController implements ActionListener, MouseListener{
     private SignUpView view;
 
     public static void main (String[] args) {
         SignUpView signUpView = new SignUpView();
         signUpView.setVisible(true);
     }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -24,4 +27,28 @@ public class SignUpViewController implements ActionListener {
         }
     }
 
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
--- src/presentation/view/SignUpView.java
+++ src/presentation/view/SignUpView.java
@@ -1,16 +1,17 @@
 package presentation.view;
 
 import presentation.JImagePanel;
+import presentation.controller.SignUpViewController;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 
 public class SignUpView extends JFrame {
     public static final String MALE_COMMAND = "MALE_COMMAND";
     public static final String FEMALE_COMMAND = "FEMALE_COMMAND";
     public static final String OTHER = "OTHER_COMMAND";
+
+    private SignUpViewController controller;
 
     public SignUpView () {
         setLocationRelativeTo(null);
@@ -51,11 +52,13 @@ public class SignUpView extends JFrame {
         textIntroPanel.setLayout(new BoxLayout(textIntroPanel, BoxLayout.Y_AXIS));
         textIntroPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+        // Añadimos todos los label y textFields
         textIntroPanel.add(setLabelAndTextField("Username", "User"));
         textIntroPanel.add(setLabelAndTextField("E-Mail", "E-Mail"));
         textIntroPanel.add(setLabelAndTextField("Account password", "Password"));
         textIntroPanel.add(setLabelAndTextField("Confirmation password", "Password"));
 
+        // Añadimos label y RadioButton que preguntan el genero
         JLabel genre =  new JLabel("Introduce your genre");
         textIntroPanel.add(genre);
         genre.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -65,16 +68,15 @@ public class SignUpView extends JFrame {
         JRadioButton jrb2 = new JRadioButton("Woman");
         jrb2.setForeground(Color.MAGENTA);
         jrb2.setActionCommand(FEMALE_COMMAND);
-        //JRadioButton jrb3 = new JRadioButton("Non-binary");
-        //jrb3.setForeground(Color.MAGENTA);
-        JRadioButton jrb4 = new JRadioButton("Other");
-        jrb4.setForeground(Color.MAGENTA);
-        jrb4.setActionCommand(OTHER);
+        JRadioButton jrb3 = new JRadioButton("Other");
+        jrb3.setForeground(Color.MAGENTA);
+        jrb3.setActionCommand(OTHER);
+
         ButtonGroup group = new ButtonGroup();
-        group.add(jrb1); group.add(jrb2); /*group.add(jrb3);*/ group.add(jrb4);
+        group.add(jrb1); group.add(jrb2);  group.add(jrb3);
 
         JPanel auxiliar = new JPanel();
-        auxiliar.add(jrb1); auxiliar.add(jrb2); /*auxiliar.add(jrb3);*/ auxiliar.add(jrb4);
+        auxiliar.add(jrb1); auxiliar.add(jrb2);  auxiliar.add(jrb3);
         textIntroPanel.add(auxiliar);
 
         return textIntroPanel;
@@ -83,7 +85,6 @@ public class SignUpView extends JFrame {
 
     private JPanel setLabelAndTextField (String label, String tField) {
         JPanel panel = new JPanel();
-        //panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setSize(new Dimension(450, 100));
 
         JLabel labelText = new JLabel(label);
@@ -93,28 +94,6 @@ public class SignUpView extends JFrame {
 
         if (!label.equals("Account password") && !label.equals("Confirmation password")) {
             JTextField fieldText = new JTextField(35);
-            fieldText.setPreferredSize(new Dimension(300, 40));
-            fieldText.setText(tField);
-            fieldText.setAlignmentX(Component.CENTER_ALIGNMENT);
-            fieldText.addFocusListener(new FocusListener() {
-                @Override
-                public void focusGained(FocusEvent e) {
-                    fieldText.setText("");
-                }
-
-                @Override
-                public void focusLost(FocusEvent e) {
-                    // Si lo han dejado vacío, volvemos a poner valor original
-                    if (fieldText.equals("")) {
-                        fieldText.setText(tField);
-                    } else {
-                        fieldText.setText(fieldText.getText());
-                    }
-                }
-            });
-            panel.add(fieldText);
-        } else {
-            JPasswordField fieldText = new JPasswordField(35);
             fieldText.setPreferredSize(new Dimension(300, 40));
             fieldText.setText(tField);
             fieldText.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -137,10 +116,14 @@ public class SignUpView extends JFrame {
             });
             */
             panel.add(fieldText);
+        } else {
+            JPasswordField fieldText = new JPasswordField(35);
+            fieldText.setPreferredSize(new Dimension(300, 40));
+            fieldText.setText(tField);
+            fieldText.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+            panel.add(fieldText);
         }
-
-
-
 
         return panel;
     }
@@ -150,13 +133,13 @@ public class SignUpView extends JFrame {
         panel.setBounds(550, 650, 330, 70);
         panel.setBackground(Color.blue);
 
-
         JLabel labelText = new JLabel("\nRegister");
-        //labelText.setHorizontalAlignment(0);
-        //labelText.setVerticalAlignment((int) JPanel.TOP_ALIGNMENT);
         labelText.setFont(new Font("Serif", Font.PLAIN, 30));
         labelText.setForeground(Color.white);
         labelText.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        labelText.addMouseListener(controller);
+
         panel.add(labelText);
         return panel;
     }
@@ -169,9 +152,12 @@ public class SignUpView extends JFrame {
          JLabel accountAlready = new JLabel("Already have an account?");
          accountAlready.setHorizontalAlignment(0);
          accountAlready.setFont(new Font("Tahoma", Font.PLAIN, 16));
+
          JLabel login = new JLabel("Log in");
          login.setHorizontalAlignment(0);
          login.setFont(new Font("Tahoma", Font.BOLD, 16));
+         login.addMouseListener(controller);
+
          panel.add(accountAlready);
          panel.add(login);
 
