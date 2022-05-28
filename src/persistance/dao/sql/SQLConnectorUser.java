4c3d06ad21393d287505c11a83609d95ce776353
borjariva <97692758+borjariva@users.noreply.github.com>
2022-05-28 19:29:18 +0200

    listas canciones

--- src/persistance/dao/sql/SQLConnectorUser.java
+++ src/persistance/dao/sql/SQLConnectorUser.java
@@ -7,7 +7,7 @@ import java.sql.*;
 import java.util.LinkedList;
 
 public class SQLConnectorUser implements UserDAO {
-    private static String dbURL = "jdbc:mysql://localhost:3306/espotifay";
+    private static String dbURL = "jdbc:mysql://localhost:3306/espotifai";
     private static String username = "root";
     private static String password = "";
     private static Connection conn;
